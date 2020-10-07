60f8915017a27e9ea10d9ee88f9a4d5f6747afff
kamal Jyotwal <kkjyotwal100@gmail.com>
2020-10-07 19:57:25 +0530

    working form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import FormComponent from "./FormComponent";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={require("./logo.png")} alt="" className="logo" />
+        <div className="buttonContainer">
+          <a className="aLink">New to Applylance?</a>
+          <a className="signupButton ">Sign Up</a>
+        </div>
+      </header>  <FormComponent />
     </div>
   );
 }
